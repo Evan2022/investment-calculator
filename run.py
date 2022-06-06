4a061c0a83449ee39647b1812d6806b46faa71f9
Evan2022 <evan201198@hotmail.com>
2022-06-06 15:10:37 +0000

    Added in user input questions

--- run.py
+++ run.py
@@ -4,3 +4,11 @@ import math
 import pyfiglet
 
 print(pyfiglet.figlet_format("Investment Calculator", justify = "center", font = "slant"))
+
+# User input questions 
+starting_balance = int(input("Please enter your starting balance: "))
+interest_rate = float(input("Please enter the interest rate: "))
+increment_rate = input("Increment timeframe (daily, weekly, monthly, yearly: ")
+additional_deposit = int(input("Additional deposit at each increment stage (optional): "))
+compound_period = int(input("Investment period length in years: "))
+calculate = input("Would you like to calculate?: ")
