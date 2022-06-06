26d004ed8024bece85acb89242a28980cba7267c
Evan2022 <evan201198@hotmail.com>
2022-06-06 15:19:47 +0000

    Add correct calcualtion for interest rate entered and if statement calculation based on increment rate entered

--- run.py
+++ run.py
@@ -5,10 +5,23 @@ import pyfiglet
 
 print(pyfiglet.figlet_format("Investment Calculator", justify = "center", font = "slant"))
 
-# User input questions 
+# User input questions, need to add restrictions on inputs
 starting_balance = int(input("Please enter your starting balance: "))
 interest_rate = float(input("Please enter the interest rate: "))
-increment_rate = input("Increment timeframe (daily, weekly, monthly, yearly: ")
+increment_rate = input("Increment timeframe (daily, weekly, monthly, yearly): ")
 additional_deposit = int(input("Additional deposit at each increment stage (optional): "))
 compound_period = int(input("Investment period length in years: "))
 calculate = input("Would you like to calculate?: ")
+
+# Calculating the correct rate as a decimal
+real_interest_rate = interest_rate * 0.01
+
+# If statement based on the users increment_rate input
+if increment_rate == "daily":
+    increment_rate = 365
+elif increment_rate == "weekly":
+    increment_rate = 52
+elif increment_rate == "monthly":
+    increment_rate = 12
+else:
+    increment_rate = 1
